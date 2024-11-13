5e6b5bbc9870f2c1981c2f8b1f272b310cf31075
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2024-11-13 09:25:53 -0300

    Adicionando aula PostComments

---
+++ CSharp/PostComents/Program.cs
@@ -0,0 +1,25 @@
+﻿using PostComents.Entities;
+using System;
+
+namespace PostComents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Incrível!");
+            Comment c2 = new Comment("Muito massa!");
+            Post p1 = new Post(
+                DateTime.ParseExact("13/11/2024 08:39:55", "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture),
+                "Boss GT1 Review Completo",
+                "Review muito massa dessa pedaleira da Boss",
+                34
+            );
+
+            p1.addComment(c1);
+            p1.addComment(c2);
+
+            Console.WriteLine(p1);
+        }
+    }
+}
