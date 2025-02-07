e9fbebee1c2509cc3635f7632863acdb63ae15f0
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-07 07:32:12 -0300

    adicionando aula herança

---
+++ CSharp/Heranca/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
