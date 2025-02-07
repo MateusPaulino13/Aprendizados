50026282f68de16f39446f2a7be7ae0df714bdfb
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-07 08:00:12 -0300

    Downcasting e upcasting

--- CSharp/Heranca/Program.cs
+++ CSharp/Heranca/Program.cs
@@ -3,12 +3,40 @@ using Heranca.Entities;
 
 namespace Heranca
 {
-    internal class Program
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
-            Console.WriteLine(account.Balance);
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //upcasting
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1004, "Mateus", 0.0, 250.0);
+            Account acc3 = new SavingAccount(1005, "Ana", 0.0, 0.01);
+
+            //downcasting
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+            //acc2.Loan(200.0); da erro pq o compilador n sabe q ela era um business account
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3; | vai dar erro em tempo de execucao pq acc3 é uma saving account
+
+            //verificando se a variavel é uma instancia de uma classe
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan! Loan!");
+            }
+
+            if (acc3 is SavingAccount)
+            {
+                SavingAccount acc5 = (SavingAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update Balance");
+            }
         }
     }
 }
