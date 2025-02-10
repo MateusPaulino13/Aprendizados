999704929eda7f0478dac60ccce32e87dab57757
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-10 07:33:00 -0300

    adicionando base do exercicio

---
+++ CSharp/EmplooyerOutsourced/Entities/Emplooyee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmplooyerOutsourced.Entities
+{
+    class Emplooyee
+    {
+        public string Name { get; private set; }
+        public int Hours { get; private set; }
+        public double valuePerHour { get; private set; }
+
+        public Emplooyee()
+        {
+        }
+
+        public Emplooyee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            this.valuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return valuePerHour * Hours;
+        }
+    }
+}
---
+++ CSharp/EmplooyerOutsourced/Entities/OutsourcedEmplooyee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmplooyerOutsourced.Entities
+{
+    class OutsourcedEmplooyee : Emplooyee
+    {
+        public double AdditionalCharge { get; private set; }
+
+        public OutsourcedEmplooyee()
+        {
+        }
+
+        public OutsourcedEmplooyee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + (1.1 * AdditionalCharge);
+        }
+    }
+}
