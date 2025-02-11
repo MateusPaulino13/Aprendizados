9566137fa5d603b3c4234cbdb3db485c435192d1
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-11 10:42:34 -0300

    Resolução do exercicio

--- CSharp/ProductUsedImported/Program.cs
+++ CSharp/ProductUsedImported/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using ProductUsedImported.Entites;
+using System;
+using System.Collections.Generic;
 
 namespace ProductUsedImported
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ta funcionando ó");
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Product> products = new List<Product>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (type == 'i' || type == 'I')
+                {
+                    Console.Write("Custom fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                } else if(type == 'u' || type == 'U')
+                {
+                    Console.Write("Manufacture Date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+                    products.Add(new UsedProduct(name, price, manufactureDate));
+                } else if(type == 'c' || type == 'C')
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Price Tags: ");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.priceTag());
+            }
         }
     }
 }
