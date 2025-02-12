13ea6cfd16c5ea0519a2a749822262a1026fad0f
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-12 14:37:45 -0300

    Base para o exercicio de execoes personalizadas

---
+++ CSharp/ExecoesPersonalizadas/Entites/Reservation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecoesPersonalizadas.Entites
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            // ve a diferença entra as datas em timespan
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            // retorna a diferença em dias com double
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return $"Room {RoomNumber}, check-in: {CheckIn.ToString("dd/MM/yyyy")}, check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }   
+    }
+}
