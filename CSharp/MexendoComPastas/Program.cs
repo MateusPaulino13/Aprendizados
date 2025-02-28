0b75847cc1240bd321044676b6a06199cc57f2e5
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-28 09:38:12 -0300

    mexendo com pastas

---
+++ CSharp/MexendoComPastas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MexendoComPastas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\mateus.paulino\OneDrive - Adventistas\Documentos\myFolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders: ");
+
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.WriteLine("|---------------------|");
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch(IOException e){
+                Console.WriteLine("Error found");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
