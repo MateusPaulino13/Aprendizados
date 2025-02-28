6a4a85bf36370c72296da2e43ac06ba2c8760b7d
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-02-28 09:50:26 -0300

    Mexendo com a classe Path

---
+++ CSharp/MexendoComPath/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MexendoComPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\mateus.paulino\OneDrive - Adventistas\Documentos\myFolder\book2.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }
+}
