23abe89fbced48d3196db4b9a5a11bcff9aa4f77
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-03-07 09:19:16 -0300

    Solução do Problema SEM interfaces

---
+++ CSharp/Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using Interfaces.Entites;
+using Interfaces.Services;
+using System;
+using System.Globalization;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+
+            Console.Write("Car Model :");
+            string carModel = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm) : ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy hh:mm) : ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per Hour : ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per Day : ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice :");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
