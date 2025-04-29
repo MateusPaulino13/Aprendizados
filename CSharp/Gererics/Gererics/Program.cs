0c9f0f3fdee57122d6f118b69206193ed0b752fc
IATec - Mateus Gabriel Santos Paulino <mateus.paulino@iatec.com>
2025-04-28 22:41:49 -0300

    resto do projeto

---
+++ CSharp/Gererics/Gererics/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Gererics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // com uma classe, metodo ou interface usando o GENERICS
+            // precisa que ela mostre o qual tipo ela vai receber, como abaixo:
+            PrintService<int> ps = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                ps.AddValue(x);
+            }
+            
+            // como o generics eu tenho segurando com Type safeting, então me dará erro em tempo de compilação
+            // string a = (string)ps.First();
+            
+            ps.Print();
+            Console.WriteLine($"First : {ps.First()}");
+        }
+    }
+}
