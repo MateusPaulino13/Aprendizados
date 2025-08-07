1f5e9636677b1b1254683a01c1c2b854ffa6f24c
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-08-07 16:53:55 -0300

    aula 1 de hashSet e SortedSet - Introdução

---
+++ CSharp/HashSetSortedSet/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HashSetSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conjunto = new HashSet<string>();
+
+            conjunto.Add("Les Paul");
+            conjunto.Add("Vox ac30");
+            conjunto.Add("Valeton gp-200");
+
+            foreach (var conj in conjunto)
+            {
+                Console.WriteLine(conj);
+            }
+        }
+    }
+}
