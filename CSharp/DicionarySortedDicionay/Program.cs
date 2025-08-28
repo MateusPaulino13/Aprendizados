945d9eb81c3b727393cd293e27507be529f6bde0
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-08-28 16:58:42 -0300

    aula des dicionarios

---
+++ CSharp/DicionarySortedDicionay/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DicionarySortedDicionay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Mateus Paulino";
+            cookies["email"] = "Mateusgpaulino@gmail.com";
+            cookies["phone"] = "19999873551";
+
+            // ou no lugar desse KeyValuePair pode colocar um VAR pra não ficar t]ao verboso
+            foreach (KeyValuePair<string, string> cookie in cookies)
+            {
+                Console.WriteLine($"{cookie.Key} : {cookie.Value}");
+            }
+        }
+    }
+}
