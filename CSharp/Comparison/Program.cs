588fb4da31a5b1a8ce8a5b3221fa1a8f04ed3992
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-08-29 09:46:38 -0300

    codigo base aula Comparison, com problema de não ficar fechada para alteração

---
+++ CSharp/Comparison/Program.cs
@@ -0,0 +1,27 @@
+﻿using Comparison.Entites;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var produtcs = new List<Product>();
+
+            produtcs.Add(new Product("Seizi Stratocaster", 3500.00));
+            produtcs.Add(new Product("Gibson SG", 9800.00));
+            produtcs.Add(new Product("Tagima Grace 7.7", 12900.90));
+
+            // ordena minha lista
+            // mas apenas se minha lista implementa a interface IComparable<>
+            produtcs.Sort();
+
+            foreach (var product in produtcs)
+            {
+                Console.WriteLine(product);
+            }
+
+        }
+    }
+}
