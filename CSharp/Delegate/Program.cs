c3fd5970a2b352c53b72612d145407206c015202
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-08-29 10:44:16 -0300

    codigo base da aula de Delegates

---
+++ CSharp/Delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using Delegate.Services;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 3;
+
+            double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
