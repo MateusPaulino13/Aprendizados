cf958c123233d113560f2d95bc7c4f1b74f50c06
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-08-29 11:58:21 -0300

    codigo base aula Linq

---
+++ CSharp/Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Três passos pra trabalhar com LINQ
+            //1º - Criar um data source (coleção, array, recurso de Entrada/Saída, etc)
+            //2º - Definir a Query
+            //3º Executar a Query (foreach ou alguma operação terminal)
+            // Data Source -> Query -> Execution
+
+            //data source
+            int[] numbers = new int[] { 1, 2, 3 };
+
+            // query expression definition
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //execute the query
+            foreach (var x in result)
+                Console.WriteLine(x);
+        }
+    }
+}
