6073ea8e5f746b7717a84a8c4f2d01987c2b291f
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-08-29 10:54:48 -0300

    explicação e implementação de Delegates

--- CSharp/Delegate/Program.cs
+++ CSharp/Delegate/Program.cs
@@ -2,6 +2,8 @@
 
 namespace Delegate
 {
+    delegate double BinaryNumericOperation(double n1, double n2);
+
     class Program
     {
         static void Main(string[] args)
@@ -9,7 +11,14 @@ namespace Delegate
             double a = 10;
             double b = 3;
 
-            double result = CalculationService.Sum(a, b);
+            //passando a referencia do metodo do service
+            // só pode usar as funções/métodos que casam com a assinatura, não posso (neste caso) colocar uma função que recebe apenas um paramentro ou que tenha outro tipo de retorno
+            // como no caso da função Square do CalculationService
+            BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation opVerboso = new BinaryNumericOperation(CalculationService.Sum); // muito verboso;
+
+            double resultInvoke = op.Invoke(a, b); //Ele esta chamando/"Invocando" a função/método que você definiu (no nosso caso é Sum())
+            double result = op(a, b);
             Console.WriteLine(result);
         }
     }
